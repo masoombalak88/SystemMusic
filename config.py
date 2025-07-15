ea8921ead656f886fcc0a180cb2d8aa96fa51a77
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2025-07-15 11:17:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # Vars For API End Pont.
-YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## E.G https://yt.okflix.
-YT_API_KEY = getenv("YT_API_KEY" , "xbit_0000557970017716954481" ) ## get your secret key from Vikky
+YTPROXY_URL = getenv("YTPROXY_URL", ') ## contact Deep @its_deep_ydv
+YT_API_KEY = getenv("YT_API_KEY" , "" ) ## get your secret key from Vikky
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
